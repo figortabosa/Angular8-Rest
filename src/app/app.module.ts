f3412b09a5858068c00933106683ff99345ecab2
francisco.igor <figortabosa@gmail.com>
2021-08-24 16:57:05 -0300

    Primeiro Commite

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms'
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ModuleWithProviders} from  '@angular/compiler/src/core';
+import { LoginComponent } from './login/login.component';
+import { HttpInterceptorModule } from './service/header-interceptor.service';
+
+export const appRouters: Routes = [
+
+  {path : 'home', component : HomeComponent},
+  {path : 'login', component : LoginComponent},
+  {path : '', component : LoginComponent}
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(appRouters);
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routes,
+    HttpInterceptorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
